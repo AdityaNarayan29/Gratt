c33d9650dfcad833e0450fa9c8ff5cede5f3c506
AdityaNarayan2000 <29adityanarayan2000@gmail.com>
2022-05-11 19:40:36 +0530

    Stuffs Added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () =>
@@ -15,18 +20,20 @@ const App = () =>
     //         <Route exact path='/' component={ Landing } />
     //     </Fragment>
     // </Router>
+    <Provider store={store}>
+        <Router>
+            <Navbar />
+            <Alert />
+            <Routes>
+                <Route exact path='/' element={<Landing />}></Route>
+                <Route exact path='/Login' element={<Login />}></Route>
+                <Route exact path='/Register' element={<Register />}></Route>
 
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route exact path='/' element={<Landing />}></Route>
-            <Route exact path='/Login' element={<Login />}></Route>
-            <Route exact path='/Register' element={<Register />}></Route>
-
-            {/* <Route exact path='/update' element={<Update />}>
+                {/* <Route exact path='/update' element={<Update />}>
             <Route path = "courses" element={<herm/>}/>
           </Route> */}
-            {/* <Route exact path="/hey" element={<Navigate replace to = "/update" />}></Route> */}
-        </Routes>
-    </Router>
+                {/* <Route exact path="/hey" element={<Navigate replace to = "/update" />}></Route> */}
+            </Routes>
+        </Router>
+    </Provider>
 export default App;
